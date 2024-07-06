a37bd03f70732863661aa26279486ee94093f61a
mikish_ash@hotmail.com <mikish_ash@hotmail.com>
2024-07-05 21:19:14 -0400

    Comentado pip install requiremtens y static_root movido

--- config/settings.py
+++ config/settings.py
@@ -159,13 +159,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app/static/'),
 ]
 
 if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
